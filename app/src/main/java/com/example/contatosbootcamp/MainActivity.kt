fe66ed8db95cfc5b0824cbc9d952ddb74aa0e922
Thiago Barros <t.b.lima18@gmail.com>
2022-02-28 14:24:34 -0300

    Development of the setContacts function

--- app/src/main/java/com/example/contatosbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/contatosbootcamp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.contatosbootcamp
 
+import android.content.ContentResolver
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import java.util.jar.Manifest
+import androidx.core.content.ContentResolverCompat.query
+import androidx.core.database.getStringOrNull
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_CONTACT = 1
@@ -33,6 +36,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setContacts() {
-        TODO("Not yet implemented")
+        val contactList: ArrayList<Contact> = ArrayList()
+
+        val cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        null,
+        null,
+        null,
+        null)
+
+        if (cursor != null) {
+            while (cursor.moveToNext()){
+                contactList.add(
+                    Contact(
+                        cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    )
+                )
+            }
+            cursor.close()
+        }
     }
 }
